642a18d4015b37bbd33c4fceb8de682605afd521
Asfia Fahmisan <138280634+Afahmisan@users.noreply.github.com>
2026-09-22 18:19:58 +0700

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Day Before n Days/day-before-n-days.c
+++ Difficulty: Easy/Day Before n Days/day-before-n-days.c
@@ -1,9 +1,3 @@
 int nthDay(int d, int n) {
-    if(d >= n){
-        return d - n;
-    } else if (d < n){
-        int hasil = (n - d -1) % 7;
-        return 6 - hasil;
-    }
-    
+    return (d - (n % 7) + 7) % 7;
 }
